71199d2eb9749a52e52c5165acec3197a659a7a9
Alex Lobakov <alobakov@users.noreply.github.com>
2017-04-16 01:59:15 -0400

    NH-3985 - Fix for subsequent child sessions being returned in disposed state (#2)
    NH-3985 - Fix for subsequent child sessions being returned in disposed state

---
+++ src/NHibernate.Test/NHSpecificTest/NH3985/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NHibernate.Test.NHSpecificTest.NH3985
+{
+	public partial class Process
+	{
+		#region Extensibility Method Definitions
+
+		public override bool Equals(object obj)
+		{
+			Process toCompare = obj as Process;
+			if (toCompare == null)
+			{
+				return false;
+			}
+
+			if (Object.Equals(this.ProcessID, default(long)) &&
+				Object.Equals(toCompare.ProcessID, default(long)))
+				return ReferenceEquals(this, toCompare);
+
+			if (!Object.Equals(this.ProcessID, toCompare.ProcessID))
+				return false;
+
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = 13;
+
+			// on transient objects, use the basic GetHashCode()
+			if (Object.Equals(this.ProcessID, default(long)))
+				return base.GetHashCode();
+
+			hashCode = (hashCode * 7) + ProcessID.GetHashCode();
+			return hashCode;
+		}
+
+		#endregion
+
+		public virtual int ProcessID { get; set; }
+
+		public virtual string Name { get; set; }
+	}
+}
